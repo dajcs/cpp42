95108a9248b9473f69f102820c2595bb884a8de5
Attila Nemet <anemet@c2r7s5.42luxembourg.lu>
2025-10-31 10:01:04 +0100

    m04ex01 ok

--- m04/ex01/main.cpp
+++ m04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 21:27:58 by anemet            #+#    #+#             */
-/*   Updated: 2025/10/30 21:39:17 by anemet           ###   ########.fr       */
+/*   Updated: 2025/10/31 09:50:17 by anemet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,54 @@ void run_subject_tests()
 	}
 }
 
+void run_deep_copy_tests()
+{
+	std::cout << "\n\n--- Running Deep Copy Tests ---" << std::endl;
+
+	// --- Testing Dog Copy constructor ---
+	std::cout << "\n--- Testing Dog Copy Constructor ---" << std::endl;
+	Dog original_dog;
+	original_dog.setIdea(0, "chase the mailman");
+	original_dog.setIdea(1, "Bury the bone");
+
+	// Create a copy using the copy constructor
+	Dog copied_dog(original_dog);
+
+	std::cout << "Original Dog's first idea: " << original_dog.getIdea(0) << std::endl;
+	std::cout << "Copied Dog's first idea: " << copied_dog.getIdea(0) << std::endl;
+
+	std::cout << "\n-> Changing original dog's idea..." << std::endl;
+	original_dog.setIdea(0, "Nap on the couch");
+
+	std::cout << "Original Dog's first idea is now: " << original_dog.getIdea(0) << std::endl;
+	std::cout << "Copied Dog's first idea is still: " << copied_dog.getIdea(0) << std::endl;
+	std::cout << "-> Different ideas means the constructor copy was DEEP" << std::endl;
+
+	// --- Test Copy Assignment Operator ---
+	std::cout << "\n--- Testing Cat Copy Assignment Operator ---" << std::endl;
+	Cat cat1;
+	cat1.setIdea(0, "Plot world domination");
+
+	Cat cat2;
+	cat2.setIdea(0, "Look cute for treats");
+
+	std::cout << "Before assginment, Cat2's idea is: " << cat2.getIdea(0) << std::endl;
+	cat2 = cat1;
+	std::cout << "After assignment cat2 = cat1, Cat2's idea is: " << cat2.getIdea(0) << std::endl;
+
+	std::cout << "\n-> Changing cat1's idea..." << std::endl;
+	cat1.setIdea(0, "Knock things off the table");
+
+	std::cout << "Cat1's idea is now: " << cat1.getIdea(0) << std::endl;
+	std::cout << "Cat2's idea is still: " << cat2.getIdea(0) << std::endl;
+	std::cout << "-> Different ideas means the assignment copy was DEEP" << std::endl;
+
+	std::cout << "\n--- End of Deep Copy Tests. The Objects will be destroyed now. ---\n" << std::endl;
+}
+
 int main()
 {
 	run_subject_tests();
+	run_deep_copy_tests();
 	return 0;
 }
