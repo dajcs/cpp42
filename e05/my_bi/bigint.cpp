03bd67dc3c0751c8b8e8cd5b886e76b91c5e6298
Attila Nemet <anemet@c2r7s5.42luxembourg.lu>
2025-11-19 15:41:26 +0100

    e05 my_bigint cont

--- e05/my_bi/bigint.cpp
+++ e05/my_bi/bigint.cpp
@@ -6,16 +6,16 @@
 /*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 16:38:19 by anemet            #+#    #+#             */
-/*   Updated: 2025/11/18 16:54:11 by anemet           ###   ########.fr       */
+/*   Updated: 2025/11/19 15:02:41 by anemet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bigint.hpp"
 
 // default constructor
-bigint::bigint()
+bigint::bigint() : str("0")
 {
-	this->str = "0";
+	// this->str = "0";
 }
 
 // parametrized constructor
@@ -31,3 +31,79 @@ bigint::bigint(const bigint& other)
 {
 	*this = other;
 }
+
+// copy assignment operator
+bigint& bigint::operator=(const bigint& other)
+{
+	if (this != &other)
+	{
+		this->str = other.str;
+	}
+	return *this;
+}
+
+// destructor
+bigint::~bigint() {}
+
+
+// --- public getter ---
+std::string bigint::getStr() const
+{
+	return this->str;
+}
+
+
+// --- addition ---
+
+// operator+
+bigint bigint::operator+(const bigint& other) const
+{
+	// create new obj temp
+	// bigint temp;
+	// temp = *this;
+	// temp += other;
+	// return temp;
+
+	return bigint (*this) += other; // reuse operator+=
+}
+
+// operator+=
+bigint& bigint::operator+=(const bigint& other)
+{
+	std::string s1 = this->str;
+	std::string s2 = other.str;
+	int i = s1.length() - 1;  // s1[i] points to the last digit of s1
+	int j = s2.length() - 1;  // s2[j] points to the last digit of s2
+	int carry = 0;
+	std::string result = "";
+
+	while (i >= 0 || j >= 0 || carry)
+	{
+		int d1 = (i >= 0) ? s1[i--] - '0' : 0; // numeric value of the digit, i--
+		int d2 = (j >= 0) ? s2[j--] - '0' : 0; // numeric value of the digit, j--
+		int sum = d1 + d2 + carry;
+		carry = sum / 10;
+		result = (char)(sum % 10) + result;
+	}
+	this->str = result;
+	return *this;
+}
+
+
+		//  ++ operators
+
+// pre-increment
+bigint& bigint::operator++()
+{
+	*this += bigint(1);
+	return *this;
+}
+
+// post-increment
+bigint  bigint::operator++(int)
+{
+	bigint temp(*this);  // create temp as a copy of current object
+	++(*this);			// increment this object
+	return temp;		// return the copy with the old value
+}
+
--- e05/my_bi/bigint.hpp
+++ e05/my_bi/bigint.hpp
@@ -6,7 +6,7 @@
 /*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:34:13 by anemet            #+#    #+#             */
-/*   Updated: 2025/11/18 16:43:13 by anemet           ###   ########.fr       */
+/*   Updated: 2025/11/19 15:39:34 by anemet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,39 @@ class bigint
 		bigint operator+(const bigint &other) const; // add, create new obj
 		bigint& operator+=(const bigint &other); // add inplace
 
-		// --- ++/-- operators
+		//  ++ operators
 		bigint& operator++(); // pre-increment
 		bigint operator++(int); // post-increment
-		bigint& operator--(); // pre-decrement
-		bigint operator--(int); // post-decrement
 
 		// --- shift with num
-		bigint operator>>(unsigned int shift) const; // right shift
-		bigint& operator>>=(unsigned int shift); // inplace right-shift
-		bigint operator<<(unsigned int shift) const; // left shift
-		bigint& operator<<=(unsigned int shift); // inplace left-shift
+		// not necessary, the compiler knows we can convert unsigned int to bigint,
+		// 		bigint(unsigned int n); // Parametrized constructor
+		// and it will use that constructor to create the obj
+
+		/*
+			We could do the reverse, define only the shift with num
+			but then we would need to have a default conversion from
+			obj -> unsigned int with operator unsigned int
+		public:
+			operator unsigned int() const;
+
+		implementation:
+		bigint::operator unsigned int() const
+		{
+			unsigned int result;
+			std::stringstream ss(this->str);
+			ss >> result;  // no protection against overflow
+			return result;
+		}
+
+		**Note**: the return type should not be specified because
+		the operator name tells the function what will be the return type.
+
+		*/
+		// bigint operator>>(unsigned int shift) const; // right shift
+		// bigint& operator>>=(unsigned int shift); // inplace right-shift
+		// bigint operator<<(unsigned int shift) const; // left shift
+		// bigint& operator<<=(unsigned int shift); // inplace left-shift
 
 		// --- shift with obj
 		bigint operator>>(const bigint& other) const; // right shift
